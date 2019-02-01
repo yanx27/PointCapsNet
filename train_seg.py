d8a037cea164677d0e8f1d2671d9db4f8da59268
benny <35829331+yanx27@users.noreply.github.com>
2019-02-01 17:34:56 +0800

    train segmentation task

--- train_seg.py
+++ train_seg.py
@@ -2,7 +2,6 @@ import argparse
 import os
 import torch
 import torch.nn.parallel
-import torch.optim as optim
 import torch.utils.data
 from collections import defaultdict
 from torch.autograd import Variable
@@ -15,7 +14,8 @@ from DataLoader import load_segdata
 from pathlib import Path
 from utils import test_seg
 from tqdm import tqdm
-
+import importlib
+import shutil
 
 seg_classes = {'Earphone': [16, 17, 18], 'Motorbike': [30, 31, 32, 33, 34, 35], 'Rocket': [41, 42, 43], 'Car': [8, 9, 10, 11], 'Laptop': [28, 29], 'Cap': [6, 7], 'Skateboard': [44, 45, 46], 'Mug': [36, 37], 'Guitar': [19, 20, 21], 'Bag': [4, 5], 'Lamp': [24, 25, 26, 27], 'Table': [47, 48, 49], 'Airplane': [0, 1, 2, 3], 'Pistol': [38, 39, 40], 'Chair': [12, 13, 14, 15], 'Knife': [22, 23]}
 seg_label_to_cat = {} # {0:Airplane, 1:Airplane, ...49:Table}
@@ -26,18 +26,18 @@ for cat in seg_classes.keys():
 
 def parse_args():
     parser = argparse.ArgumentParser('PointCapsNetSeg')
-    parser.add_argument('--batchSize', type=int, default=16, help='input batch size')
+    parser.add_argument('--batchSize', type=int, default=8, help='input batch size')
     parser.add_argument('--workers', type=int, default=4, help='number of data loading workers')
     parser.add_argument('--epoch', type=int, default=25, help='number of epochs to train for')
     parser.add_argument('--data_path', type=str, default='./data/shapenet16/', help='data path')
-    parser.add_argument('--result_dir', type=str, default='./experiment/results/',help='dir to save pictures')
-    parser.add_argument('--log_dir', type=str, default='./experiment/logs/',help='decay rate of learning rate')
+    parser.add_argument('--log_dir', type=str, default='logs/',help='decay rate of learning rate')
     parser.add_argument('--pretrain', type=str, default=None,help='whether use pretrain model')
     parser.add_argument('--train_metric', type=bool, default=False, help='Whether evaluate on training data')
     parser.add_argument('--use_vox', type=bool, default=False, help='Whether use capsnet extract voxel feature or not')
     parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
     parser.add_argument('--rotation',  default=None,help='range of training rotation')
     parser.add_argument('--n_routing_iter', type=int, default=1, help='Number if rounting iteration')
+    parser.add_argument('--model_name', type=str, default='PointCapsNetSeg', help='Name of model')
 
     return parser.parse_args()
 
@@ -46,19 +46,19 @@ def main(args):
     '''CREATE DIR'''
     experiment_dir = Path('./experiment/')
     experiment_dir.mkdir(exist_ok=True)
-    result_dir = Path(args.result_dir)
-    result_dir.mkdir(exist_ok=True)
-    checkpoints_dir = Path('./experiment/checkpoints/')
+    file_dir = Path(str(experiment_dir) +'/'+ str(datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')))
+    file_dir.mkdir(exist_ok=True)
+    checkpoints_dir = file_dir.joinpath('checkpoints/')
     checkpoints_dir.mkdir(exist_ok=True)
-    log_dir = Path(args.log_dir)
+    log_dir = file_dir.joinpath(args.log_dir)
     log_dir.mkdir(exist_ok=True)
 
     '''LOG'''
     args = parse_args()
-    logger = logging.getLogger("PointCapsNetSeg")
+    logger = logging.getLogger(args.model_name)
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler = logging.FileHandler(args.log_dir + 'train-'+ str(datetime.datetime.now().strftime('%Y-%m-%d %H-%M'))+'.txt')
+    file_handler = logging.FileHandler(str(log_dir) + '/train_%s.txt'%args.model_name)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
@@ -83,7 +83,11 @@ def main(args):
     blue = lambda x: '\033[94m' + x + '\033[0m'
 
     USE_VOXEL_FEATURE = args.use_vox
-    model = PointNetSeg(k=num_classes,n_routing=args.n_routing_iter,use_vox_feature=USE_VOXEL_FEATURE)
+    model_module = importlib.import_module(args.model_name)
+    shutil.copy('./%s.py'%model_module.__name__, str(file_dir))
+
+    model = model_module.PointNetSeg(k=num_classes,n_routing=args.n_routing_iter,use_vox_feature=USE_VOXEL_FEATURE)
+    # model = PointNetSeg(k=num_classes,n_routing=args.n_routing_iter,use_vox_feature=USE_VOXEL_FEATURE)
     if args.pretrain is not None:
         model.load_state_dict(torch.load(args.pretrain))
         print('load model %s'%args.pretrain)
@@ -97,7 +101,7 @@ def main(args):
     #optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
     optimizer = torch.optim.Adam(
         model.parameters(),
-        lr=0.001,
+        lr=5e-4,
         betas=(0.9, 0.999),
         eps=1e-08,
         weight_decay=1e-4
@@ -138,7 +142,7 @@ def main(args):
                  epoch, 'test', test_metrics['accuracy'],test_metrics['iou']))
         if test_metrics['accuracy'] > best_acc:
             best_acc = test_metrics['accuracy']
-            torch.save(model.state_dict(), '%s/PointCapsSeg_%.3d_%.4f.pth' % (checkpoints_dir, epoch, best_acc))
+            torch.save(model.state_dict(), '%s/PointCapsSeg4_%.3d_%.4f.pth' % (checkpoints_dir, epoch, best_acc))
             logger.info('Save model..')
             print('Save model..')
 
