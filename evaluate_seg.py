358c100fc4d0557cecd837f817b45c484ca6d7f4
benny <35829331+yanx27@users.noreply.github.com>
2019-02-13 16:54:22 +0800

    test segmentation task

--- evaluate_seg.py
+++ evaluate_seg.py
@@ -1,3 +1,4 @@
+#-*-coding:GBK -*-
 import argparse
 import os
 import torch
@@ -17,6 +18,7 @@ from utils import test_seg
 from tqdm import tqdm
 import importlib
 import shutil
+import sys
 
 seg_classes = {'Earphone': [16, 17, 18], 'Motorbike': [30, 31, 32, 33, 34, 35], 'Rocket': [41, 42, 43], 'Car': [8, 9, 10, 11], 'Laptop': [28, 29], 'Cap': [6, 7], 'Skateboard': [44, 45, 46], 'Mug': [36, 37], 'Guitar': [19, 20, 21], 'Bag': [4, 5], 'Lamp': [24, 25, 26, 27], 'Table': [47, 48, 49], 'Airplane': [0, 1, 2, 3], 'Pistol': [38, 39, 40], 'Chair': [12, 13, 14, 15], 'Knife': [22, 23]}
 seg_label_to_cat = {} # {0:Airplane, 1:Airplane, ...49:Table}
@@ -79,7 +81,8 @@ def main(args):
     blue = lambda x: '\033[94m' + x + '\033[0m'
 
     USE_VOXEL_FEATURE = args.use_vox
-    model_module = importlib.import_module(model_name)
+    shutil.copy('%s/%s.py' % (experiment_dir,model_name), str(os.getcwd())+'/OldModel.py')
+    model_module = importlib.import_module('OldModel')
     model = model_module.PointNetSeg(k=num_classes,
                                      n_routing=args.n_routing_iter,
                                      use_vox_feature=USE_VOXEL_FEATURE,
